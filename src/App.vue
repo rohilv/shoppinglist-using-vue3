5b350d69cc3b0da2fe2b41347ee1ae86c4f05fc1
Rohil <rohilvisariya69@gmail.com>
2023-01-08 23:50:17 +0530

    Async actions using Pinia

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,16 @@
 <script setup>
 import TheHeader from "@/components/TheHeader.vue";
 import ProductCard from "@/components/ProductCard.vue";
-import products from "@/data/products.json";
+import { useProductStore } from "./stores/ProductStores";
+const productStore = useProductStore();
+productStore.fill();
 </script>
 
 <template>
   <div class="container">
     <TheHeader />
     <ul class="sm:flex flex-wrap lg:flex-nowrap gap-5">
-      <ProductCard
-        v-for="product in products"
-        :key="product.name"
-        :product="product"
-      />
+      <ProductCard v-for="product in productStore.products" :key="product.name" :product="product" />
     </ul>
   </div>
 </template>
--- src/main.js
+++ src/main.js
@@ -6,14 +6,18 @@ import FontAwesomePlugin from "./plugins/FontAwesome";
 import "./assets/main.pcss";
 
 // App Wide Components
+import { createPinia } from "pinia";
 import AppButton from "./components/AppButton.vue";
 import AppCountInput from "./components/AppCountInput.vue";
 import AppModalOverlay from "./components/AppModalOverlay.vue";
 
+const pinia = createPinia();
 // Init App
 createApp(App)
   .use(FontAwesomePlugin)
+  .use(pinia)
   .component("AppButton", AppButton)
   .component("AppCountInput", AppCountInput)
   .component("AppModalOverlay", AppModalOverlay)
   .mount("#app");
+
